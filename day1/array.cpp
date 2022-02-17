0e130b47bd2b4aeb8992997442c1edc31f571c0e
ayush <nitaandroid121@gmail.com>
2022-02-17 21:41:58 +0530

    Day1 code

---
+++ day1/array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n ;
+
+    n = 10;
+    int arr[n];     
+   
+    return 0;
+}
---
+++ day1/basic_variable_demo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+
+    // int   - Number 1, 3, -5, 0, 3456
+    int i = 1; // 4
+    float j = 10.5556; // 4
+    double j1 = 2345.126; //8
+    char k = '1'; // 1   --> 128---127 
+    bool isOdd = 1;
+
+    std::cout << j1;
+
+    return 0;
+}
